f1b4943f797d795720c14e52e1d9fde657dc084b
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-12 10:27:17 -0500

    Actually use the generated instances

--- tuple-transmission/detail/operations/DeserializeAndInvoke.h
+++ tuple-transmission/detail/operations/DeserializeAndInvoke.h
@@ -21,7 +21,8 @@
 #define INCLUDED_DeserializeAndInvoke_h_GUID_252a9b27_ba20_4cd5_84ba_a399a673ed92
 
 // Internal Includes
-// - none
+#include "DeserializeOverloads_Generated.h"
+#include "../types/IntegralTypes.h"
 
 // Library/third-party includes
 // - none
@@ -32,9 +33,32 @@
 namespace transmission {
 	namespace detail {
 		namespace operations {
+			/*
+				namespace impl {
+					template<typename SerializationPolicy, typename MessageType, typename Function, typename Iterator>
+					struct DeserializeFunctorWrapper {
+						public:
+							DeserializeFunctorWrapper(Function & func, Iterator & iter) : f(func), it(iter) {}
+
+							template<typename MessageSize>
+							void operator()(MessageSize const& m) {
+								generated::deserialize<SerializationPolicy, MessageType>(f, it, m);
+							}
+						private:
+							Function & f;
+							Iterator & it;
+					}
+
+
+				} // end of namespace impl
+				*/
 			template<typename SerializationPolicy, typename MessageType, typename Function, typename Iterator>
 			void deserializeAndInvoke(Function & f, Iterator & it) {
-			
+//				typedef impl::DeserializeFunctorWrapper<SerializationPolicy, MessageType, Function, Iterator> WrapperType;
+//				WrapperType fWrapper(f, it);
+
+				generated::deserialize<MessageType, SerializationPolicy>(f, it);
+//				runtime_int_to_type_impl< uint8_t, generated::DeserializeMaxArity, WrapperType &>::apply(
 			}
 		} // end of namespace operations
 	} // end of namespace detail
