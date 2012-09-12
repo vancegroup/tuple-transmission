7520eb4ffffdc4092438d378eb0fc766950c0b53
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-12 12:44:49 -0500

    Clean up receiver and handle multiple messages in a single append.

--- tuple-transmission/Receiver.h
+++ tuple-transmission/Receiver.h
@@ -31,12 +31,21 @@
 // - none
 
 namespace transmission {
+
+	/** @brief Base class for functors that react to messages.
+
+		Uses the "curiously-recurring-template-pattern" - pass your deriving
+		class name as the first template argument, and your message collection
+		as the second.
+
+		You will need to write a void operator()( [...] ) for each message
+		type in the collection.  If you intend to ignore some messages,
+		see transmission::PartialReceiver.
+	*/
 	template<typename Derived, typename MessageCollection>
 	class Receiver {
 		private:
 			typedef ReceiveHandler<MessageCollection> receive_handler_type;
-			typedef typename MessageCollection::message_types message_types;
-			typedef typename MessageCollection::envelope_type::serialization_policy serialization_policy;
 		public:
 			typedef typename receive_handler_type::buffer_size_type buffer_size_type;
 			typedef void result_type;
@@ -45,19 +54,35 @@ namespace transmission {
 				return _recv.bufferAvailableSpace();
 			}
 
+			/// @brief Function to append additional data received from your
+			/// data source and trigger message processing.
+			///
+			/// Returns the number of messages processed.
 			template<typename InputIterator>
-			void appendReceived(InputIterator input_begin, InputIterator input_end) {
+			uint8_t appendReceived(InputIterator input_begin, InputIterator input_end) {
 				_recv.bufferAppend(input_begin, input_end);
+				return processMessages();
+			}
+
+		private:
+			uint8_t processMessages(uint8_t msgCount = 0) {
 				if (_recv.checkBufferForMessage()) {
+					typedef typename MessageCollection::envelope_type::serialization_policy serialization_policy;
+					typedef typename MessageCollection::message_types message_types;
+
 					detail::operations::deserializeAndInvoke<message_types, serialization_policy>(
 					    _recv.getCurrentMessageId(),
 					    getDerived(),
 					    _recv.getDataIterator()
 					);
-				}
-			}
+					// Remove handled message
+					_recv.popMessage();
 
-		private:
+					// Repeat until no more.
+					return processMessages(msgCount + 1);
+				}
+				return msgCount;
+			}
 			receive_handler_type _recv;
 			Derived & getDerived() {
 				return *(static_cast<Derived*>(this));
