21ab11b3d7ec8d4dbe630c183d860c39328951e3
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-12 16:36:08 -0500

    Dummy header for arduino.

---
+++ tuple-transmission.h
@@ -0,0 +1,32 @@
+/** @file
+	@brief Header
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef INCLUDED_tuple_transmission_h_GUID_40d8ea93_aa3b_4885_bd5e_c2fad6a1b903
+#define INCLUDED_tuple_transmission_h_GUID_40d8ea93_aa3b_4885_bd5e_c2fad6a1b903
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+#endif // INCLUDED_tuple_transmission_h_GUID_40d8ea93_aa3b_4885_bd5e_c2fad6a1b903
