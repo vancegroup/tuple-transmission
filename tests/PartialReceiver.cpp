bd41e499afda3068e29e4449aa30fcaa8547ef68
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-12 12:46:57 -0500

    Move shared tests into one header

--- tests/PartialReceiver.cpp
+++ tests/PartialReceiver.cpp
@@ -49,35 +49,6 @@ class TestReceiver : public transmission::PartialReceiver<TestReceiver, MyMessag
 };
 
 
-void appendValidMessageCharacters(TestReceiver & recv, std::size_t n, std::size_t start = 0) {
-	BOOST_TEST_MESSAGE("Inserting valid message elements [" << start << ", " << start + n - 1 << "]");
-	BOOST_ASSERT(start + n <= ValidMessage.size());
-	recv.appendReceived(ValidMessage.begin() + start, ValidMessage.begin() + start + n);
-}
 
-BOOST_AUTO_TEST_CASE(DefaultConstruction) {
-	BOOST_REQUIRE_NO_THROW(TestReceiver());
-	TestReceiver r;
-	BOOST_CHECK_EQUAL(r.first, 0);
-	BOOST_CHECK_EQUAL(r.second, 0);
-	BOOST_CHECK_EQUAL(r.third, 0);
-}
-
-BOOST_AUTO_TEST_CASE(IncompleteMessages) {
-	TestReceiver r;
-	for (unsigned long n = 1; n < ValidMessage.size(); ++n) {
-		appendValidMessageCharacters(r, n);
-		BOOST_CHECK_EQUAL(r.first, 0);
-		BOOST_CHECK_EQUAL(r.second, 0);
-		BOOST_CHECK_EQUAL(r.third, 0);
-	}
-}
-
-BOOST_AUTO_TEST_CASE(CompleteMessage) {
-	TestReceiver r;
-	appendValidMessageCharacters(r, ValidMessage.size());
-	BOOST_CHECK_EQUAL(r.first, 5);
-	BOOST_CHECK_EQUAL(r.second, 10);
-	BOOST_CHECK_EQUAL(r.third, 15);
-}
+#include "ReceiverCommon.h"
 
