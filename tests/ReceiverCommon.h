b47735e33dbd4d5c05af68d6d12f39c731b519c7
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-13 11:08:21 -0500

    Add round-tripping tests

--- tests/ReceiverCommon.h
+++ tests/ReceiverCommon.h
@@ -1,5 +1,8 @@
 
 using transmission::BoundMessageType;
+using transmission::transmitters::VectorBuffer;
+
+#include <tuple-transmission/transmitters/VectorBuffer.h>
 
 inline void appendValidMessageCharacters(TestReceiver & recv, std::size_t n, std::size_t start = 0) {
 	BOOST_TEST_MESSAGE("Inserting valid message elements [" << start << ", " << start + n - 1 << "]");
@@ -33,14 +36,37 @@ BOOST_AUTO_TEST_CASE(CompleteMessage) {
 	BOOST_CHECK_EQUAL(r.third, 15);
 }
 
+BOOST_AUTO_TEST_CASE(CompleteMessageRoundtrip) {
+	VectorBuffer<MyMessageCollection> buf;
+	send<MyMessageCollection, MessageB>(buf, 5, 10, 15);
+
+	TestReceiver r;
+	r.appendReceived(buf.begin(), buf.end());
+	BOOST_CHECK_EQUAL(r.first, 5);
+	BOOST_CHECK_EQUAL(r.second, 10);
+	BOOST_CHECK_EQUAL(r.third, 15);
+}
 
 BOOST_AUTO_TEST_CASE(DifferentMessageSameSignature) {
+	/// This actually tests checksum failure on the ChecksumReceiver suite,
+	/// since we're modifying a stock message rather than roundtripping.
 	TestReceiver r;
 	typedef BoundMessageType<MyMessageCollection, MessageD> BoundMessageD;
 
 	/// Change the ID to MessageD which has the same signature.
 	ValidMessageBufferType buf = ValidMessage;
 	buf[MessageIdIndex] = BoundMessageD::message_id();
+	r.appendReceived(buf.begin(), buf.end());
+	BOOST_CHECK_EQUAL(r.first, 0);
+	BOOST_CHECK_EQUAL(r.second, 0);
+	BOOST_CHECK_EQUAL(r.third, 0);
+}
+
+BOOST_AUTO_TEST_CASE(DifferentMessageSameSignatureRoundtrip) {
+	VectorBuffer<MyMessageCollection> buf;
+	send<MyMessageCollection, MessageD>(buf, 5, 10, 15);
+
+	TestReceiver r;
 	r.appendReceived(buf.begin(), buf.end());
 	BOOST_CHECK_EQUAL(r.first, 0);
 	BOOST_CHECK_EQUAL(r.second, 0);
