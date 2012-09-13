9a720f9467611b897fe9971653daf6c9622adff7
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-13 11:25:14 -0500

    make astyle be noisier again.

--- astyle-project.sh
+++ astyle-project.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 PROJECT=$(cd $(dirname $0) && pwd)
-astyle -n --formatted --recursive "${PROJECT}/tuple-transmission/*.h" "${PROJECT}/tests/*.cpp"
-astyle -n --formatted --exclude=ProtocolForTest.h --recursive "${PROJECT}/tests/*.h"
+astyle -n --recursive "${PROJECT}/tuple-transmission/*.h" "${PROJECT}/tests/*.cpp"
+astyle -n --exclude=ProtocolForTest.h --recursive "${PROJECT}/tests/*.h"
 if which lua-styler > /dev/null; then
 	lua-styler --nobackup ${PROJECT}/generation/*.lua
 fi
