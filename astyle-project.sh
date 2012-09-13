adf305573a6b2ee95e13ea866bff41e31a5fd7f3
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-13 10:54:52 -0500

    Update astyle script to handle all test .h's except the one it mangles.

--- astyle-project.sh
+++ astyle-project.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 PROJECT=$(cd $(dirname $0) && pwd)
-astyle -n --recursive "${PROJECT}/tuple-transmission/*.h" "${PROJECT}/tests/*.cpp"
+astyle -n --formatted --recursive "${PROJECT}/tuple-transmission/*.h" "${PROJECT}/tests/*.cpp"
+astyle -n --formatted --exclude=ProtocolForTest.h --recursive "${PROJECT}/tests/*.h"
 if which lua-styler > /dev/null; then
 	lua-styler --nobackup ${PROJECT}/generation/*.lua
 fi
