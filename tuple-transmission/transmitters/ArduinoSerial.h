208012f5279b6cfeb6ca8da2c3bb373d5bb02c33
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-04 13:28:14 -0500

    Start some integration headers

---
+++ tuple-transmission/transmitters/ArduinoSerial.h
@@ -0,0 +1,32 @@
+/** @file
+	@brief Header
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef INCLUDED_ArduinoSerialTransmitter_h_GUID_83d9f433_13ac_4b1d_88b1_8ef673dfd648
+#define INCLUDED_ArduinoSerialTransmitter_h_GUID_83d9f433_13ac_4b1d_88b1_8ef673dfd648
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+#endif // INCLUDED_ArduinoSerialTransmitter_h_GUID_83d9f433_13ac_4b1d_88b1_8ef673dfd648
