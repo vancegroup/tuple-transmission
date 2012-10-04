00f2630d6ec73c8ff0c89c88d260e70357165d43
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-04 12:41:27 -0500

    Clean up and document

--- tuple-transmission/Receiver.h
+++ tuple-transmission/Receiver.h
@@ -55,7 +55,6 @@ namespace transmission {
 			typedef Receiver<MessageCollection> receiver_type;
 		public:
 			typedef typename receive_buffer_type::buffer_size_type buffer_size_type;
-			typedef void result_type;
 
 			/// @brief Constructor that takes a reference to your handler,
 			/// and stores a pointer to it.
@@ -70,6 +69,9 @@ namespace transmission {
 				: _invoker(&receiver_type::deserializeAndInvoke<MessageFunctor>)
 				, _functorPointer(&handler) {}
 
+
+			/// @brief Returns the number of bytes of available space in
+			/// the buffer.
 			buffer_size_type getBufferAvailableSpace() const {
 				return _recv.bufferAvailableSpace();
 			}
@@ -109,24 +111,21 @@ namespace transmission {
 			/// Returns the number of messages processed.
 			uint8_t processMessages() {
 				return processMessagesImpl();
-				//return (*_processor)(*this, 0);
 			}
 
 		private:
+			/// @brief Internal function to recursively process messages.
+			///
+			/// When it finds a full one, it calls through the function
+			/// pointer to invoke the handler.
 			uint8_t processMessagesImpl(uint8_t msgCount = 0) {
 				if (_recv.checkBufferForMessage()) {
+					const MessageIdType id = _recv.getCurrentMessageId();
+					// Record this message as the last handled ID
+					_lastMessageId = id;
 
-					_lastMessageId = _recv.getCurrentMessageId();
-
-
-					/*
-					detail::operations::deserializeAndInvoke<message_types, serialization_policy>(
-					    _recv.getCurrentMessageId(),
-					    *static_cast<MessageFunctorType *>(_functorPointer),
-					    _recv.getDataIterator()
-					);
-					*/
-					(*_invoker)(_recv.getCurrentMessageId(), _functorPointer, _recv.getDataIterator());
+					// Invoke the handler on this message.
+					(*_invoker)(id, _functorPointer, _recv.getDataIterator());
 
 					// Remove handled message
 					_recv.popMessage();
@@ -137,13 +136,16 @@ namespace transmission {
 				return msgCount;
 			}
 
-			typedef void (*InvokerFnPtr)(MessageIdType, void *, buffer_const_iterator);
 
-			/// @brief Static function template to recursively process messages.
+			/// @brief Static function template to invoke the handler on a
+			/// message we already know is there.
 			///
 			/// Instantiated with knowledge of the message functor's type so
 			/// it can safely cast it back from a void * - this is the only
 			/// code that actually requires knowledge of the message functor.
+			///
+			/// Doesn't technically need to be a member, but it makes it easier
+			/// because we don't have to repeat types as template parameters.
 			template<typename MessageFunctorType>
 			static void deserializeAndInvoke(MessageIdType msgID, void * functorPointer, buffer_const_iterator it) {
 				typedef typename MessageCollection::envelope_type::serialization_policy serialization_policy;
@@ -155,10 +157,22 @@ namespace transmission {
 				);
 			}
 
+			/// @brief Typedef for a function pointer to the signature of deserializeAndInvoke -
+			/// note that it does not depend on MessageFunctorType since it takes a void *
+			/// and can safely cast that back inside.
+			typedef void (*InvokerFnPtr)(MessageIdType, void *, buffer_const_iterator);
 
+			/// @brief Internal receive buffer wrapper object, used by this class
+			/// and by the envelope.
 			receive_buffer_type _recv;
-			//MessageFunctor _functor;
+
+			/// @brief Pointer to the invoker function that knows the real,
+			/// static type of _functorPointer
 			InvokerFnPtr _invoker;
+
+			/// @brief A pointer to the user-supplied handler functor, stored
+			/// as void * to not require parameterization of this class,
+			/// and passed to _invoker which knows what it really is.
 			void * _functorPointer;
 			boost::optional<MessageIdType> _lastMessageId;
 	};
