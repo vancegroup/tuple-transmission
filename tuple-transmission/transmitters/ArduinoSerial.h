d5dddd819ab8245f5e4991398e6113fd82d38829
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-08 14:20:59 -0500

    Implementation for arduino transmitters.

--- tuple-transmission/transmitters/ArduinoSerial.h
+++ tuple-transmission/transmitters/ArduinoSerial.h
@@ -21,12 +21,37 @@
 #define INCLUDED_ArduinoSerialTransmitter_h_GUID_83d9f433_13ac_4b1d_88b1_8ef673dfd648
 
 // Internal Includes
-// - none
+#include "../detail/bases/TransmitterBase.h"
 
 // Library/third-party includes
 // - none
 
 // Standard includes
 // - none
+
+#ifdef ARDUINO
+namespace transmission {
+	namespace transmitters {
+		/// @addtogroup Transmitters Transmitter Classes
+		class ArduinoSerial : public TransmitterBase<ArduinoSerial> {
+			public:
+				ArduinoSerial(Print & stream) : s(stream) {}
+
+				void writeByte(uint8_t v) {
+					s.write(v);
+				}
+
+				void write(uint8_t * data, std::size_t len) {
+					s.write(data, len);
+				}
+
+			private:
+				Stream & s;
+		};
+		/// @}
+	} // end of namespace transmitters
+} // end of namespace transmission
+
+#endif
 
 #endif // INCLUDED_ArduinoSerialTransmitter_h_GUID_83d9f433_13ac_4b1d_88b1_8ef673dfd648
