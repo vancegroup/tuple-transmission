1dc7510a5040dab1282b3033f21796b588b22084
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-08 14:20:59 -0500

    Implementation for arduino transmitters.

--- tuple-transmission/transmitters/ArduinoI2C.h
+++ tuple-transmission/transmitters/ArduinoI2C.h
@@ -21,12 +21,50 @@
 #define INCLUDED_ArduinoI2CTransmitter_h_GUID_a948b207_8b92_41e2_ac13_9020d01dc412
 
 // Internal Includes
-// - none
+#include "../detail/bases/TransmitterBase.h"
+#include "BoostArrayBuffer.h"
 
 // Library/third-party includes
 // - none
 
 // Standard includes
 // - none
+
+#ifdef ARDUINO
+namespace transmission {
+	namespace transmitters {
+		/// @addtogroup Transmitters Transmitter Classes
+
+		/// @brief Arduino I2C transmitter - internally buffers because
+		/// some uses of the I2C library in Arduino allow only a single
+		/// write.
+		class ArduinoI2C : public TransmitterBase<ArduinoI2C> {
+			public:
+				ArduinoSerial(TwoWire & stream) : s(stream) {}
+
+				static const uint8_t BUFSIZE = 255;
+
+				void writeByte(uint8_t v) {
+					bufTx.output(v);
+				}
+
+				void write(uint8_t * data, std::size_t len) {
+					bufTx.output(data, len);
+				}
+
+				void writeFinished() {
+					s.write(bufTx.begin(), bufTx.end() - bufTx.begin());
+					bufTx.reset();
+				}
+
+			private:
+				BoostArrayBuffer<BUFSIZE> bufTx;
+				TwoWire & s;
+		};
+		/// @}
+	} // end of namespace transmitters
+} // end of namespace transmission
+
+#endif
 
 #endif // INCLUDED_ArduinoI2CTransmitter_h_GUID_a948b207_8b92_41e2_ac13_9020d01dc412
