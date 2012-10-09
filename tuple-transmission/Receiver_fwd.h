5ec1555b178a9f9ad22c6221265924d3389ee5d2
Ryan Pavlik <rpavlik@iastate.edu>
2012-10-09 13:19:37 -0500

    Add a forward declaration header for Receiver

---
+++ tuple-transmission/Receiver_fwd.h
@@ -0,0 +1,37 @@
+/** @file
+	@brief Header
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef INCLUDED_Receiver_fwd_h_GUID_b09d1fc3_5c93_416b_b8cb_5a3ec25f0209
+#define INCLUDED_Receiver_fwd_h_GUID_b09d1fc3_5c93_416b_b8cb_5a3ec25f0209
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+namespace transmission {
+	template<typename MessageCollection>
+	class Receiver;
+} // end of namespace transmission
+
+#endif // INCLUDED_Receiver_fwd_h_GUID_b09d1fc3_5c93_416b_b8cb_5a3ec25f0209
